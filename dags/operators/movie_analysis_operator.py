37076b34557fb9410279d3908544645a71f869e5
Ravindra Saragadam <okabe@Ravindras-MacBook-Air-2.local>
2024-10-29 11:43:01 +0530

    improved pipelines logic

--- dags/operators/movie_analysis_operator.py
+++ dags/operators/movie_analysis_operator.py
@@ -3,37 +3,21 @@ import pandas as pd
 import configparser
 import os
 import psycopg2
-import json
 
-# Load configurations
-CONFIG_PATH = "CONFIG_PATH"
-config = configparser.ConfigParser()
-storage_config_location = os.path.abspath(os.environ.get(CONFIG_PATH, 'configs/')+'storage_config.ini')
-paths_config_location = os.path.abspath(os.environ.get(CONFIG_PATH, 'configs/')+'paths_config.ini')
-config.read(storage_config_location)
-config.read(paths_config_location)
-print(storage_config_location)
-STORAGE_TYPE = config['General']['storage_type']
-OUTPUT_FILE_PATH = config['Paths']['output_file_path'] 
-
-# PostgreSQL connection details
-POSTGRES_HOST = config['Postgres']['postgres_host']
-POSTGRES_USER = config['Postgres']['postgres_user']
-POSTGRES_PASSWORD = config['Postgres']['postgres_password']
-POSTGRES_DATABASE = config['Postgres']['postgres_database']
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost:5347"),
+POSTGRES_USER = os.getenv("POSTGRES_USER", "airflow"),
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "airflow"),
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE", "airflow")
 
 class MovieAnalysisOperator(BaseOperator):
-    def __init__(self, movie_task_type=None, postgres_conn_id='postgres_default', *args, **kwargs):
+    def __init__(self, movie_task_type=None, *args, **kwargs):
         super(MovieAnalysisOperator, self).__init__(*args, **kwargs)
         self.movie_task_type = movie_task_type
-        self.postgres_conn_id = postgres_conn_id
-        
         # Load configuration
         config = configparser.ConfigParser()
-        paths_config_location = os.path.abspath(os.environ.get(CONFIG_PATH, 'configs/')+'paths_config.ini')
-        config.read(paths_config_location)
-        print(paths_config_location)
-        
+        CONFIG_PATH = os.getenv("CONFIG_PATH", "configs/")
+        PATHS_CONFIG_PATH = os.path.join(CONFIG_PATH, 'paths_config.ini')
+        config.read(PATHS_CONFIG_PATH)
         self.user_data_path = config['Paths']['user_data_path']  # Path to u.user file
         self.ratings_data_path = config['Paths']['ratings_data_path']  # Path to u.data file
         self.movies_data_path = config['Paths']['movies_data_path']  # Path to u.item file
@@ -48,10 +32,6 @@ class MovieAnalysisOperator(BaseOperator):
             self.find_mean_age()
         elif self.movie_task_type == "top_rated_movies":
             self.find_top_rated_movies()
-        elif self.movie_task_type == "top_genres":
-            self.find_top_genres()
-        elif self.movie_task_type == "similar_movies":
-            self.find_similar_movies()
 
     def save_to_postgres(self, table_name, df):
         """Save DataFrame to PostgreSQL with defined schema."""
@@ -82,22 +62,6 @@ class MovieAnalysisOperator(BaseOperator):
                 rating_count INTEGER
             )
             """
-        elif table_name == 'top_genres':
-            create_table_query = f"""
-            CREATE TABLE IF NOT EXISTS top_genres (
-                id SERIAL PRIMARY KEY,
-                genre VARCHAR(255),
-                count INTEGER
-            )
-            """
-        elif table_name == 'similar_movies':
-            create_table_query = f"""
-            CREATE TABLE IF NOT EXISTS similar_movies (
-                id SERIAL PRIMARY KEY,
-                movie_title VARCHAR(255),
-                similar_movies JSONB
-            )
-            """
         cursor.execute(create_table_query)
         
         # Insert data
@@ -108,12 +72,6 @@ class MovieAnalysisOperator(BaseOperator):
             elif table_name == 'top_rated_movies':
                 insert_query = f"INSERT INTO {table_name} (movie_id, title, avg_rating, rating_count) VALUES (%s, %s, %s, %s)"
                 cursor.execute(insert_query, (row['movie_id'], row['title'], row['avg_rating'], row['rating_count']))
-            elif table_name == 'top_genres':
-                insert_query = f"INSERT INTO {table_name} (genre, count) VALUES (%s, %s)"
-                cursor.execute(insert_query, (row['genre'], row['count']))
-            elif table_name == 'similar_movies':
-                insert_query = f"INSERT INTO {table_name} (movie_title, similar_movies) VALUES (%s, %s)"
-                cursor.execute(insert_query, (row['movie_title'], row['similar_movies']))
 
         conn.commit()
         cursor.close()
@@ -155,49 +113,3 @@ class MovieAnalysisOperator(BaseOperator):
             self.save_to_postgres('top_rated_movies', top_movies_with_titles)
         except Exception as e:
             self.log.error(f"Error in find_top_rated_movies: {e}")
-
-    def find_top_genres(self):
-        try:
-            ratings_df = pd.read_csv(self.ratings_data_path, sep='\t', header=None, encoding='ISO-8859-1')
-            movies_df = pd.read_csv(self.movies_data_path, sep='|', header=None, encoding='ISO-8859-1')
-
-            ratings_df.columns = ['user_id', 'movie_id', 'rating', 'timestamp']
-            movies_df.columns = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url'] + self.genre_names
-            
-            # Prepare data for genre analysis
-            genres = movies_df.columns[5:]  # Adjust this index based on your actual data
-            genres_df = movies_df.melt(id_vars=['movie_id', 'title'], value_vars=genres,
-                                        var_name='genre', value_name='is_genre')
-            top_genres_df = genres_df[genres_df['is_genre'] == 1].groupby('genre').size().reset_index(name='count')
-            top_genres_df = top_genres_df.nlargest(10, 'count')
-
-            self.log.info(top_genres_df)  # Log the result
-            
-            # Save to PostgreSQL
-            self.save_to_postgres('top_genres', top_genres_df)
-        except Exception as e:
-            self.log.error(f"Error in find_top_genres: {e}")
-
-    def find_similar_movies(self):
-        try:
-            # Load movies data
-            movies_df = pd.read_csv(self.movies_data_path, sep='|', header=None, encoding='ISO-8859-1')
-            movies_df.columns = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url'] + self.genre_names
-            
-            # Find similar movies based on genre
-            genre_columns = movies_df.columns[5:]  # Adjust this index based on your actual data
-            similar_movies_list = []
-            for _, row in movies_df.iterrows():
-                similar_movies = movies_df[movies_df[genre_columns].eq(row[genre_columns]).any(axis=1)]
-                similar_movies_list.append((row['title'], similar_movies[['title']].to_dict(orient='records')))
-            
-            # Convert list to DataFrame
-            similar_movies_df = pd.DataFrame(similar_movies_list, columns=['movie_title', 'similar_movies'])
-            
-            # Convert 'similar_movies' column to JSON strings
-            similar_movies_df['similar_movies'] = similar_movies_df['similar_movies'].apply(json.dumps)
-            
-            # Save to PostgreSQL
-            self.save_to_postgres('similar_movies', similar_movies_df)
-        except Exception as e:
-            self.log.error(f"Error in find_similar_movies: {e}")
--- dags/pipeline2_movie_analysis.py
+++ dags/pipeline2_movie_analysis.py
@@ -1,17 +1,21 @@
 from airflow import DAG
-from datetime import timedelta
+from datetime import timedelta, datetime
+from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
 from operators.movie_analysis_operator import MovieAnalysisOperator
+from operators.movies_recommendations_operator import MovieSimilarityOperator
+from operators.genere_operator import UserOccupationGenreAnalysisOperator
 import logging
 
+# Default arguments for the DAG
 default_args = {
     'owner': 'airflow',
-    'start_date': days_ago(1),
-    'email_on_failure': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'start_date': days_ago(1),  # Start date for the DAG
+    'email_on_failure': False,    # Disable email alerts on failure
+    'retries': 1,                 # Number of retries before failing
+    'retry_delay': timedelta(minutes=1),  # Delay between retries
 }
 
 def check_pipeline1_success(**kwargs):
@@ -26,11 +30,12 @@ def check_pipeline1_success(**kwargs):
         print("Pipeline 1 did not succeed.")
         return False
 
+# Define the DAG
 with DAG(
     'pipeline2_movie_analysis',
     default_args=default_args,
-    schedule_interval='0 20 * * 1-5',
-    catchup=False,
+    schedule_interval='0 20 * * 1-5',  # Schedule to run at 20:00 from Monday to Friday
+    catchup=False,  # Do not perform backfill
 ) as dag:
 
     check_pipeline1 = PythonOperator(
@@ -39,30 +44,33 @@ with DAG(
         provide_context=True,
     )
 
+    # Task to find the mean age of movie watchers
     find_mean_age_task = MovieAnalysisOperator(
         movie_task_type='mean_age',
         task_id='find_mean_age',
-        execution_timeout=timedelta(minutes=10)  # Set a timeout for the task
+        execution_timeout=timedelta(minutes=10)  
     )
 
+    # Task to find the top-rated movies
     find_top_rated_movies_task = MovieAnalysisOperator(
         movie_task_type='top_rated_movies',
         task_id='find_top_rated_movies',
         execution_timeout=timedelta(minutes=10)
     )
 
-    find_top_genres_task = MovieAnalysisOperator(
-        movie_task_type='top_genres',
+    # Task to find the top genres based on user occupation
+    find_top_genres_task = UserOccupationGenreAnalysisOperator(
         task_id='find_top_genres',
         execution_timeout=timedelta(minutes=10)
     )
 
-    find_similar_movies_task = MovieAnalysisOperator(
-        movie_task_type='similar_movies',
+    # Task to find similar movies
+    find_similar_movies_task = MovieSimilarityOperator(
         task_id='find_similar_movies',
         execution_timeout=timedelta(minutes=10)
     )
 
+    # Dummy task to signify the end of the pipeline
     end = DummyOperator(task_id='end')
 
     # Set the task dependencies
